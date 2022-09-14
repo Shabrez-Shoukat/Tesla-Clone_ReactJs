9ea55833e0f0894a269c2408efbb5c15986b2e49
Shabrez Shoukat <shabrez.shoukat@ascendbpo.us>
2022-09-14 13:22:50 -0700

    ADD: Redux for cars

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 import styled from "styled-components";
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
 
 function Header() {
   const [burgerStatus, setBurgerStatus] = useState(false);
+  const cars = useSelector(selectCars);
+  console.log(cars);
 
   return (
     <Container>
@@ -12,10 +16,9 @@ function Header() {
         <img src="/images/logo.svg" alt=""/>
       </a>
       <Menu>
-        <a href="#">Model S</a>
-        <a href="#">Model 3</a>
-        <a href="#">Model X</a>
-        <a href="#">Model Y</a>
+          {cars && cars.map((car, index) => 
+          <a key={index} href='#'>{car}</a>
+          )}
       </Menu>
         <RightMenu>
           <a href="#">Shop</a>
@@ -26,14 +29,14 @@ function Header() {
         <CloseWrapper>
           <CustomClose onClick={ () => setBurgerStatus(false)}/>
         </CloseWrapper>
+        {cars && cars.map((car, index) => 
+          <li key={index}><a href='#'>{car}</a></li>
+          )}
         <li><a href='#'>Existing Inventory</a></li>
         <li><a href='#'>Used Inventory</a></li>
         <li><a href='#'>Trade-in</a></li>
         <li><a href='#'>Cybertruck</a></li>
         <li><a href='#'>Roadaster</a></li>
-        <li><a href='#'>Existing Inventory</a></li>
-        <li><a href='#'>Existing Inventory</a></li>
-        <li><a href='#'>Existing Inventory</a></li>
       </BurgerNav>
     </Container>
   )
